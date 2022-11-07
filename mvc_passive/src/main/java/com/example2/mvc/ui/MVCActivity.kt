22c9f4da6011ed59f9a77f27c040b65fc25a245a
2015techinstitute <2015techinstitute@gmail.com>
2022-11-07 22:51:19 +0530

    Memory leak test by screen orientation change

--- mvc_passive/src/main/java/com/example2/mvc/ui/MVCActivity.kt
+++ mvc_passive/src/main/java/com/example2/mvc/ui/MVCActivity.kt
@@ -1,12 +1,14 @@
 package com.example2.mvc.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example2.mvc.data.PostDataFromApi
 import com.example2.mvc.data.PostRepository
 import com.example2.mvc.data.PostRepositoryImpl
 import com.example2.mvc.databinding.ActivityMvcBinding
 import com.example2.mvc.network.RetrofitService
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
@@ -27,7 +29,11 @@ class MVCActivity : AppCompatActivity() {
             runBlocking {
                 launch {
                     val result = repository.getPostById(2)
+                    delay(1000L)
                     val posts = result.body() as PostDataFromApi
+                    delay(1000L)
+                    Toast.makeText(this@MVCActivity, posts.toString(), Toast.LENGTH_SHORT).show()
+                    delay(1000L)
                     binding.txtPost.text = posts.toString()
                 }
             }
